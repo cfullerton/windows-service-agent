5781b23d2d8145011bc92dfb4b7538cf85466013
Conner Fullerton <cfullerton@alkamitech.com>
2018-03-05 21:03:49 -0600

    Add project files.

---
+++ agent/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agent
+{
+    public partial class agent : ServiceBase
+    {
+        public agent()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Set up a timer to trigger every minute.  
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 60 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.  
+            WebRequest webRequest = WebRequest.Create("http://dev.sarifas.com:3000/update-node");
+            WebResponse webResp = webRequest.GetResponse();
+
+        }
+        protected override void OnStop()
+        {
+        }
+    }
+}
