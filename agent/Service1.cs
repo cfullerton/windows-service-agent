5c2e2c7a21d682f098fca4f90f90354445e84f76
Conner Fullerton <cfullerton@alkamitech.com>
2018-03-06 21:40:35 -0600

    added fields

--- agent/Service1.cs
+++ agent/Service1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Net;
 using System.ServiceProcess;
 using System.Text;
@@ -21,16 +22,39 @@ namespace agent
         protected override void OnStart(string[] args)
         {
             // Set up a timer to trigger every minute.  
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 60000; // 60 seconds  
+            System.Timers.Timer timer = new System.Timers.Timer
+            {
+                Interval = 60000 // 60 seconds  
+            };
             timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
             timer.Start();
         }
         public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
         {
-            var url = "http://foo.bar:8080";
-            // TODO: Insert monitoring activities here.  
-            WebRequest webRequest = WebRequest.Create(requestUriString: url);
+            var baseUrl = "http://foo.bar:8080";
+            var queryString = "";
+            queryString += "?systemname" + System.Environment.MachineName;
+
+            //initialize the select query with command text
+            System.Management.SelectQuery query = new SelectQuery(@"Select * from Win32_ComputerSystem");
+
+            //initialize the searcher with the query it is supposed to execute
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(query))
+            {
+                //execute the query
+                foreach (ManagementObject process in searcher.Get())
+                {
+                    //print system info
+                    process.Get();
+                
+                    queryString += "&Manufacturer=" + process["Manufacturer"];
+                    queryString += "&Model=" + process["Model"];
+                    queryString += "&UserName=" + process["UserName"];
+
+                }
+            }
+
+            WebRequest webRequest = WebRequest.Create(requestUriString: baseUrl + queryString);
             WebResponse webResp = webRequest.GetResponse();
 
         }
